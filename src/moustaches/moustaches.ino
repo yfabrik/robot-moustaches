fca2e49b16da7c8129cee026c0229d5821820d5f
yfabrik <138105766+yfabrik@users.noreply.github.com>
2023-10-05 08:30:03 +0200

    moteur qui réagi correct aux collision

--- src/moustaches/moustaches.ino
+++ src/moustaches/moustaches.ino
@@ -1,61 +1,60 @@
 // For two motors instance at once
 #include <L298NX2.h>
 
-#define EN_A 7
+#define EN_A 9
 #define IN1_A 8
-#define IN2_A 9
+#define IN2_A 7
+
 #define EN_B 10
-#define IN1_B 11
-#define IN2_B 12
+#define IN1_B 5
+#define IN2_B 6
 
 L298NX2 myMotors(EN_A, IN1_A, IN2_A, EN_B, IN1_B, IN2_B);
 
-#define moustacheGauche 10
-#define moustacheDroite 11
+#define moustacheGauche 3
+#define moustacheDroite 4
 
-int tournant = 500;
-boolean manoeuvre = false;
+
 
 void setup() {
   pinMode(moustacheGauche, INPUT_PULLUP);
   pinMode(moustacheDroite, INPUT_PULLUP);
 
-  myMotors.setSpeed(100);
+  //motor speed si alim en 6v -> 255 = 6V ; 127 = 3v
+  myMotors.setSpeed(250);
 }
 
 void loop() {
-
-  myMotors.forward();
-
   if (digitalRead(moustacheGauche) == HIGH && digitalRead(moustacheDroite) == HIGH) {
-    //motor run
+    myMotors.forward();
+
   } else if (digitalRead(moustacheGauche) == HIGH && digitalRead(moustacheDroite) == LOW) {
-    if (!manoeuvre) {
-      myMotors.backwardFor(2000);
-      myMotors.backwardForA(1000);
-      myMotors.forwardForB(1000);
-
-
-      manoeuvre = true;
-    }
-
     // back and turn left
+    myMotors.stop();
+    delay(1000);
+    myMotors.backward();
+    delay(1000);
+    myMotors.stop();
+    delay(1000);
+    myMotors.backwardA();
+    myMotors.forwardB();
+    delay(500);
   } else if (digitalRead(moustacheGauche) == LOW && digitalRead(moustacheDroite) == HIGH) {
     //back and turn right
-    if (!manoeuvre) {
-      myMotors.backwardFor(2000);
-      myMotors.backwardForB(1000);
-      myMotors.forwardForA(1000);
-
-
-      manoeuvre = true;
-    }
+    myMotors.stop();
+    delay(1000);
+    myMotors.backward();
+    delay(1000);
+    myMotors.stop();
+    delay(1000);
+    myMotors.backwardB();
+    myMotors.forwardA();
+    delay(500);
+    // }
   } else {
     //back
-    if (!manoeuvre) {
-      myMotors.backwardFor(2000);
+    myMotors.backwardFor(2000);
 
-      manoeuvre = true;
-    }
+    // }
   }
 }
